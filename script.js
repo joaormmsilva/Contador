a0ecfd91e4e10a7c28ee03f4292da58cd382230f
João Renato Moreira Mendes da Silva <joaormmsilva@gmail.com>
2025-05-09 16:13:26 -0300

    add

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 let numero = document.getElementById("num")
 numero.textContent = 0
 
+function atualizarCor() {
+    let valor = parseInt(numero.textContent);
+    if (valor > 0) {
+        numero.style.color = "green";
+    } else if (valor < 0) {
+        numero.style.color = "red";
+    } else {
+        numero.style.color = "black";
+    }
+}
+
 function aumentar(){
     let valorAtual = parseInt(numero.textContent);
     numero.textContent = valorAtual + 1;
+    atualizarCor()
 }
 
 function diminuir(){
     let valorAtual = parseInt(numero.textContent);
     numero.textContent = valorAtual + -1;
+    atualizarCor()
 }
 
 function limpar(){
     numero.textContent = 0
+    atualizarCor()
 }
